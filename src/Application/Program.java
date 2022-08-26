680dda92e05082683ea6d302d7d2ab0a27ed1886
Brenner Alexandria <obrenneralexandria@gmail.com>
2022-08-26 19:13:29 -0300

    Finally

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -31,7 +31,7 @@ public class Program {
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
 				System.out.println();
-				System.out.println("Target: ");
+				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
